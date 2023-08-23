dda4da5ef7d5128affb973ba85ec566432f2fac7
abhi2666 <95623413+abhi2666@users.noreply.github.com>
2023-08-23 21:06:48 +0530

    new code

--- townJudge.cpp
+++ townJudge.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        int hash[n+1] = {0};
-        for(int i = 0; i < trust.size(); i++)
+        vector<int>hash(n + 1, 0);
+        for(auto it : trust)
         {
-            hash[trust[i][0]] = -1;
-            if(hash[trust[i][1]] != -1)
-            {
-                hash[trust[i][1]]++;
-            }
+            hash[it[0]]--;
+            hash[it[1]]++;
         }
-
-        for(int i = 0; i < n + 1; i++)
+        for(auto h : hash)
         {
-            if(hash[i] > 0) return i;
+            if(h == n-2) return h;
         }
-
         return -1;
     }
 };
