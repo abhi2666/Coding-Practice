62c66d4b99fe83bb272d7a1aa78ef0001da8cd31
abhi2666 <95623413+abhi2666@users.noreply.github.com>
2023-09-07 12:08:42 +0530

    updated validSudoku

--- validSudoku.cpp
+++ validSudoku.cpp
@@ -11,44 +11,31 @@ class Solution
 public:
     bool isValidSudoku(vector<vector<char>> &board)
     {
-        // we will check for row, columns and 3*3 grid
-        // rows
-        map<char, int> mp;
-        for (int i = 0; i < 9; i++)
+        // we will put our data into a set in the form of a string
+        // like row-rowNo-value, col-colno-value, grid-gridno-value
+        // and through this we can do the code in single pass 
+        // to calculate which grid does the element belongs.. we can use formula-
+        // (row/3)*3+col/3
+        // if we don't find the elements in set then we will put them in and if we do find the
+        // elements then it means there is a duplicate element - return false
+        unorderd_set<string>st;
+        for(int i = 0; i < 9; i++)
         {
-            mp.clear();
-            for (int j = 0; j < 9; i++)
+            for(int j = 0; j < 9; j++)
             {
-                if (mp.count(board[i][j]) > 0)
-                    return false;
-                mp[board[i][j]] = 1;
-            }
-        }
-
-        // now check for columns
-        for (int i = 0; i < 9; i++)
-        {
-            mp.clear();
-            for (int j = 0; j < 9; j++)
-            {
-                if (mp.count(board[j][i]) > 0)
-                    return false;
-                mp[board[i][j]] = 1;
-            }
-        }
-
-        // for 3 * 3 grid
-        vector<unordered_set<char>> check_subbox(9);
-        for (int row = 0; row < 9; row++)
-        {
-            for (int col = 0; col < 9; col++)
-            {
-                int loc = (row / 3) * 3 + col / 3;
-                if (board[row][col] != '.' && check_subbox[loc].find(board[row][col]) != check_subbox[loc].end())
+                if(board[i][j] != '.')
                 {
-                    return false;
+                    string row = "row" + to_string(i)+to_string(board[i][j]);
+                    string col = "col" + to_string(j)+to_string(board[i][j]);
+                    string grid = "grid" + to_string((i/3)*3(j/3))+to_string(board[i][j]);
+                    // check if any of these are already present or nots
+                    if(st.find(row) != st.end() || st.find(col) != st.end() || st.find(grid) != st.end()) return false;
+                    // add the elements into set
+                    st.insert(row);
+                    st.insert(col);
+                    st.insert(grid);
+
                 }
-                check_subbox[loc].insert(board[row][col]);
             }
         }
         return true;
